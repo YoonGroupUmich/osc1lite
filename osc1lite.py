YoonGroupUmich/osc1lite

commit 669b2663d3e2cebf61161a3b81a98eb62c7c4c6a

API clean up
Remove the deprecated ext_trig parameter
Store the calibration data as part of the OSC1Lite object

osc1lite.py
@@ -15,7 +15,7 @@ class SquareWaveform(Waveform):
 
     def __init__(self, mode=0, amp=0., pw=0., period=0.):
         """
-        mode controls the rising/falling edge width
+        mode: controls the rising/falling edge width
         mode       rising edge width (ms)
           0                 0
           1                 0.1
@@ -23,12 +23,16 @@ class SquareWaveform(Waveform):
           3                 1
           4                 2
 
-        amp controls the wave amplitude
+        amp: controls the wave amplitude, unit: uA, range: [0, 155.9].
+        amp=0 can be used to stop the channel
+
+        period: controls the wave period, unit: second, range: [0, 17.98].
+        pw: controls the wave pulse width, unit: second, range: [0, period]
         """
-        self.mode = mode  # mode controls the rising/falling edge
-        self.amp = amp  # uA, range: [0, 155.9]. Use amp=0 to stop the channel
-        self.period = period  # sec, range: [0, 17.98]
-        self.pulse_width = pw  # sec, range: [0, period]
+        self.mode = mode
+        self.amp = amp
+        self.period = period
+        self.pulse_width = pw
 
 
 class CustomWaveform(Waveform):
@@ -40,15 +44,13 @@ class CustomWaveform(Waveform):
 
 
 class ChannelInfo:
-    def __init__(self, waveform: SquareWaveform, n_pulses=1,
-                 ext_trig=0):
+    def __init__(self, waveform: SquareWaveform, n_pulses=1):
         """
-        mode controls the rising/falling edge width
-        amp controls the wave amplitude
+        waveform: the waveform object. Currently we only support SquareWaveform
+        n_pulses: positive integer. Number of pulses
         """
         self.wf = waveform
-        self.n_pulses = n_pulses  # set n_pulses to 0 for continuous output
-        self.ext_trig = ext_trig  # deprecated
+        self.n_pulses = n_pulses
 
 
 class OSC1Lite:
@@ -66,12 +68,16 @@ class OSC1Lite:
                 sha256.update(data)
             return sha256.hexdigest()
 
-    def __init__(self, dev: ok.okCFrontPanel):
+    def __init__(self, dev: ok.okCFrontPanel, calib):
         if not dev.IsOpen():
             logging.getLogger('OSC1Lite').fatal('Device is not opened')
             raise AssertionError('Device is not opened')
         self.dev = dev
         self.device_lock = threading.RLock()
+        if calib is None:
+            self.calib = [None for _ in range(12)]
+        else:
+            self.calib = calib
 
     def configure(self, bit_file='OSC1_LITE_Control.bit',
                   ignore_hash_error=False):
@@ -173,9 +179,10 @@ class OSC1Lite:
             self._write_to_wire_in(0x00, 1)
             self._write_to_wire_in(0x00, 0)
 
-    def set_channel(self, channel, data: ChannelInfo, calib=None):
+    def set_channel(self, channel, data: ChannelInfo):
         logging.getLogger('OSC1Lite').debug(
-            'Sending params for channel %d: mode=%d, amp=%.1f, pw=%f, period=%f', channel,
+            'Sending params for channel %d: mode=%d, amp=%.1f, pw=%f, '
+            'period=%f', channel,
             data.wf.mode, data.wf.amp, data.wf.pulse_width, data.wf.period)
         word = round(data.wf.pulse_width / (2 ** 7) * self._freq)
         word_pw = 0 if word < 0 else 0xffff if word > 0xfffff else word
@@ -191,11 +198,11 @@ class OSC1Lite:
             self._write_to_wire_in(
                 0x0a, (word_pw >> 16) | ((word_period >> 8) & 0x0f00),
                 update=False)
-            if calib is None:
+            if self.calib[channel] is None:
                 gain = 0x8000
                 zero = 0x0000
             else:
-                t10, t90 = calib
+                t10, t90 = self.calib[channel]
                 # c10 = 33, c90 = 295
                 t90 = t90 / 20 * 65536
                 t10 = t10 / 20 * 65536
@@ -205,14 +212,13 @@ class OSC1Lite:
                 zero = - b / k
             self._write_to_wire_in(0x0c, round(gain) & 0xffff, update=False)
             self._write_to_wire_in(0x0d, round(zero) & 0xffff, update=False)
-            self._write_to_wire_in(
-                0x06, data.wf.mode | (channel << 4) | (data.ext_trig << 9),
-                update=True)
+            self._write_to_wire_in(0x06, data.wf.mode | (channel << 4),
+                                   update=True)
 
             # Send data update trigger
             self._write_to_wire_in(0x06, 0x0100, mask=0x0100, update=True)
 
-    def reset(self, calib):
+    def reset(self):
         with self.device_lock:
             for channel in range(0x20):
                 self._write_to_wire_in(channel, 0, update=False)
@@ -221,7 +227,8 @@ class OSC1Lite:
             self.reset_dac()
             self.reset_pipe()
             for i in range(12):
-                self.set_channel(i, ChannelInfo(SquareWaveform()), calib[i])
+                self.set_channel(i, ChannelInfo(SquareWaveform()),
+                                 self.calib[i])
         logging.getLogger('OSC1Lite').info('Reset done')
 
     def init_dac(self):

main.py
@@ -29,11 +29,12 @@ def main():
     assert n_devices, 'No connected device. Check the connection and make sure no other program is occupying the device.'
 
     # Open the default device
-    dev.OpenBySerial("")
+    #serial = '1740000JJK'
+    serial = ''
+    dev.OpenBySerial(serial)
     assert dev.IsOpen(), 'Device open failed. Is the FPGA dead?'
 
     # Load the calibration data (you need to fill in the serial of your board)
-    #serial = '1740000JJK'
     try:
         with open('calib/' + serial + '.calib') as fp:
             calib = []
@@ -54,9 +55,9 @@ def main():
         calib = [None for _ in range(12)]
 
     # Initialize OSC1Lite board
-    osc = osc1lite.OSC1Lite(dev)
+    osc = osc1lite.OSC1Lite(dev, calib=calib)
     osc.configure(bit_file='OSC1_LITE_Control.bit', ignore_hash_error=False)
-    osc.reset(calib=calib)
+    osc.reset()
     osc.init_dac()
     osc.enable_dac_output()
 
@@ -69,7 +70,7 @@ def main():
     # Configure the waveform parameters of each channel
     for ch in range(12):
         osc.set_channel(ch, osc1lite.ChannelInfo(
-            osc1lite.SquareWaveform(0, 50, .1, .2)), calib=calib[ch])
+            osc1lite.SquareWaveform(0, 50, .1, .2)))
 
     # Send PC trigger to all channels
     osc.trigger_channel(range(12))

oscgui.py
@@ -206,7 +206,7 @@ class ChannelCtrl:
 
         wf = self.waveform_choice.GetSelection()
         data = self.mf.wfm.waveform_panels[wf].channel_info()
-        self.mf.device.set_channel(self.ch, data, self.mf.calib[self.ch])
+        self.mf.device.set_channel(self.ch, data)
         self.mf.device.set_trigger_source(self.ch, self.trigger)
         self.mf.device.set_trigger_mode(self.ch, self.continuous)
 
@@ -681,7 +681,6 @@ class MainFrame(wx.Frame):
         self.devices = {}
 
         self.board_relative_controls = []
-        self.calib = [None for _ in range(12)]
 
         p = wx.Panel(self, -1)
 
@@ -962,7 +961,7 @@ class MainFrame(wx.Frame):
                     wf = x.waveform_choice.GetSelection()
                     data = self.wfm.waveform_panels[wf].channel_info()
                     if self.device is not None:
-                        self.device.set_channel(ch, data, self.calib[ch])
+                        self.device.set_channel(ch, data)
                         if oscgui_config['OSCGUI'][
                             'channel_auto_enable'] == 'yes':
                             self.device.set_trigger_source(ch, x.trigger)
@@ -997,7 +996,27 @@ class MainFrame(wx.Frame):
                         'Device not open. Maybe you have connected to a new '
                         'board but have not restarted OSCGUI?')
                     return
-                self.device = osc1lite.OSC1Lite(self._dev)
+                try:
+                    with open('calib/' + serial + '.calib') as fp:
+                        calib = []
+                        for _ in range(12):
+                            s = next(fp).strip().split(None, 2)
+                            s[0] = float(s[0])
+                            s[1] = float(s[1])
+                            if len(s) == 3:
+                                s[2] = float(s[2])
+                                s[0] /= s[2]
+                                s[1] /= s[2]
+                            else:
+                                s[0] /= 100
+                                s[1] /= 100
+                            calib.append(s[0:2])
+                except:
+                    calib = [None for _ in range(12)]
+                    logging.getLogger('OSCGUI').warning(
+                        'Calibration data for board %s not found. '
+                        'Running in uncalibrated mode.', serial)
+                self.device = osc1lite.OSC1Lite(self._dev, calib=calib)
                 try:
                     self.device.configure(
                         bit_file='OSC1_LITE_Control.bit',
@@ -1016,27 +1035,7 @@ class MainFrame(wx.Frame):
                     self.device = None
                     self._dev.Close()
                     return
-                try:
-                    with open('calib/' + serial + '.calib') as fp:
-                        self.calib = []
-                        for _ in range(12):
-                            s = next(fp).strip().split(None, 2)
-                            s[0] = float(s[0])
-                            s[1] = float(s[1])
-                            if len(s) == 3:
-                                s[2] = float(s[2])
-                                s[0] /= s[2]
-                                s[1] /= s[2]
-                            else:
-                                s[0] /= 100
-                                s[1] /= 100
-                            self.calib.append(s[0:2])
-                except:
-                    self.calib = [None for _ in range(12)]
-                    logging.getLogger('OSCGUI').warning(
-                        'Calibration data for board %s not found. '
-                        'Running in uncalibrated mode.', serial)
-                self.device.reset(self.calib)
+                self.device.reset()
                 self.device.init_dac()
                 self.device.enable_dac_output()
                 self.Freeze()

commit 253369178c21968d9bc910113049181144ffbf15

Remove the extra calib parameter
Bug introduced in 669b2663d3e2cebf61161a3b81a98eb62c7c4c6a

osc1lite.py
@@ -226,8 +226,7 @@ class OSC1Lite:
             self.reset_dac()
             self.reset_pipe()
             for i in range(12):
-                self.set_channel(i, ChannelInfo(SquareWaveform()),
-                                 self.calib[i])
+                self.set_channel(i, ChannelInfo(SquareWaveform()))
         logging.getLogger('OSC1Lite').info('Reset done')
 
     def init_dac(self):